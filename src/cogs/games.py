7cedfb3b5f37017b1d56bd0160a744b568c7bb32
Potatopy <119952546+Potato-js@users.noreply.github.com>
2024-11-21 12:15:32 -0500

    Added a simple dice roll

---
+++ src/cogs/games.py
@@ -0,0 +1,23 @@
+import discord
+import random
+
+from discord.ext import commands
+
+
+class Games(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    @commands.command()
+    async def dice(self, ctx):
+        dice_roll = random.randint(1, 6)
+        response_embed = discord.Embed(
+            title="Dice roll 🎲",
+            description=f"You rolled a {dice_roll}"
+        )
+        response_embed.set_footer("Created by weeaboo")
+        await ctx.send("Hello")
+
+
+async def setup(bot):
+    await bot.add_cog(Games(bot))
