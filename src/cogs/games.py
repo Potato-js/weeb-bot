4033e217106ab5a5f1a2f36d6a22a035d0ed4bea
Potato-js <h3lped.taken@gmail.com>
2025-01-01 16:55:22 -0500

    Added respective cogs

--- src/cogs/games.py
+++ src/cogs/games.py
@@ -11,15 +11,19 @@ class Games(commands.Cog):
     @commands.command()
     async def dice(self, ctx):
         dice_roll = random.randint(1, 6)
+        # print(dice_roll)
         response_embed = discord.Embed(
-            title="Dice roll 🎲",
-            description=f"You rolled a {dice_roll}"
+            title="Dice roll 🎲", description=f"You rolled a {dice_roll}"
         )
-        response_embed.set_footer("Created by weeaboo")
-        # try:
-        await ctx.send(embed=response_embed)
-        # except Exception as e:
-        #     print(e)
+        response_embed.set_footer(text="Made by weeaboo")
+        try:
+            await ctx.send(embed=response_embed)
+        except Exception as e:
+            print(e)
+
+    @commands.command()
+    async def setup_counter(self, ctx):  # TODO: Make this command
+        pass
 
 
 async def setup(bot):
