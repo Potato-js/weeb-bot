ff2709c3cbc91a655f4fbc043d9204f979ece635
Potato-js <h3lped.taken@gmail.com>
2025-01-04 17:36:50 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,6 @@ import os
 from dashboard.app import create_app
 
 if __name__ == "__main__":
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  # REMOVE THIS LINE IN PRODUCTION
     app = create_app()
     app.run(debug=True)
