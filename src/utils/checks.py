c52d8a8f809563539b7e5b8484ad81c04bf30018
Potato-js <119952546+Potato-js@users.noreply.github.com>
2025-01-27 13:49:35 -0500

    Moved the FP check to `checks.py`
    Make it look less jank :)

--- src/utils/checks.py
+++ src/utils/checks.py
@@ -6,3 +6,28 @@ def is_server_owner():
         return ctx.author.id == ctx.guild.owner_id
 
     return commands.check(predicate)
+
+
+def check_perms(perm_name: str):
+    async def predicate(ctx: commands.Context) -> bool:
+        if ctx.author.guild_permissions.administrator:
+            return True
+
+        fp_cog = ctx.bot.get_cog("FakePerms")
+        if not fp_cog:
+            raise commands.CheckFailure("FakePerms Cog not activated")
+
+        role_ids = [role.id for role in ctx.author.roles]
+
+        for role_id in role_ids:
+            role_perms = fp_cog.get_role_permissions(role_id)
+            admin_flag = fp_cog.permission_flags.get("ADMINISTRATOR")
+            perm_flag = fp_cog.permission_flags.get(perm_name.upper())
+            if perm_flag and (role_perms & perm_flag):
+                return True
+            elif admin_flag and (role_perms & admin_flag):
+                return True
+
+        raise commands.MissingPermissions([perm_name])
+
+    return commands.check(predicate)
